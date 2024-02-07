45f06e2b1d23ac58c95f36014e381ba14ef28f60
baodm <baodm@vng.com.vn>
2024-02-07 14:58:17 +0700

    deploy application

--- mysql/mysql_test.go
+++ mysql/mysql_test.go
@@ -22,4 +22,11 @@ func TestFormatArrayMysql(t *testing.T) {
 		t.Fatalf("Expected %v Found %v\n", expected, res)
 	}
 
+	c := []float32{1.1, 2.2, 3.3, 4.4, 5.5}
+	res = mysql.FormatArrayMysql[float32](c)
+	expected = "(1.1, 2.2, 3.3, 4.4, 5.5)"
+	if res != expected {
+		t.Fatalf("Expected %v Found %v\n", expected, res)
+	}
+
 }
