b31c3c6ffad552247e07cd44455c4033065e0a4f
baodm <baodm@vng.com.vn>
2024-02-14 14:21:43 +0700

    remove accents

---
+++ accent/RemoveAccents.go
@@ -0,0 +1,18 @@
+package accent
+
+import (
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
+
+func RemoveAccent(s string) string {
+	s = strings.ReplaceAll(s, "Đ", "D") // Don't know why Đ|đ cannot be transformed to D|d so we need to use this
+	s = strings.ReplaceAll(s, "đ", "d")
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	res, _, _ := transform.String(t, s)
+	return res
+}
