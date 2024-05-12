11dc4d35584d2ec9f8237c8455eebfab0be29538
baodm <baodm@vng.com.vn>
2024-05-13 06:54:29 +0700

    check empty row error mysql sqlc && deploy application

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -3,20 +3,20 @@ package mysql
 import "fmt"
 
 func FormatArrayMysql[K int | int32 | int64 | string | float32 | float64](arr []K) string {
-
 	ans := "("
 	first := true
 	for _, element := range arr {
-
 		if !first {
-
 			ans = ans + ", "
 		}
-
 		ans = ans + fmt.Sprint(element)
 		first = false
 	}
 
 	ans = ans + ")"
 	return ans
+}
+
+func IsEmptyRowError(err error) bool {
+	return err.Error() == "sql: no rows in result set"
 }
